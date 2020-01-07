5f8e04d849894ea06037975c4f9f7bf58bd57a05
Matt Gahrns <matthewgahrns@hotmail.com>
2020-01-07 10:46:15 -0800

    Obtained a MapView with google maps api

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+// import firebase from 'firebase';
+import MapView from 'react-native-maps';
+import Constants from 'expo-constants';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+
+
+  render(){
+    return (
+      <View style={styles.container}>
+        {/* <Text>Open up App.js to start working on your app!</Text>
+        <Text>My Google Maps Demo</Text> */}
+        <MapView provider='google' style={styles.mapStyle} />
+      </View>
+    );
+  }
+    
 }
 
 const styles = StyleSheet.create({
@@ -15,5 +25,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
